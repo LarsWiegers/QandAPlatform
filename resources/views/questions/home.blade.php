c543ef161453ba61f793be0dd6c48032fc167dec
Lars Wiegers <larswiegers@live.nl>
2017-06-12 18:56:01 +0200

    WIP sorting

--- resources/views/questions/home.blade.php
+++ resources/views/questions/home.blade.php
@@ -25,4 +25,36 @@
         </div>
     </div>
 </div>
+<div class="panel panel-default order-by">
+    <div class="panel-heading">Order by</div>
+
+    <div class="panel-body">
+        <form class="form-horizontal" role="form" method="POST" action="{{ route('add-a-question') }}">
+            {{ csrf_field() }}
+
+
+            <div class="form-group{{ $errors->has('question') ? ' has-error' : '' }}">
+                <label for="password" class="col-md-12 control-label">Question</label>
+
+                <div class="col-md-12">
+                    <textarea id="question" class="form-control" name="question" required></textarea>
+
+                    @if ($errors->has('question'))
+                        <span class="help-block">
+                                        <strong>{{ $errors->first('question') }}</strong>
+                                    </span>
+                    @endif
+                </div>
+            </div>
+            <input type="hidden" value="{{Auth::user()->id}}" name="user_id">
+            <div class="form-group">
+                <div class="col-md-8 col-md-offset-4">
+                    <button type="submit" class="btn btn-primary">
+                        Submit
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
 @endsection
